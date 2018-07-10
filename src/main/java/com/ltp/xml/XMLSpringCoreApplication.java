147d09ea45afac96188e3a0b5a9185c33b3b1bb1
Niceug <2235422344@qq.com>
2018-07-10 19:33:27 +0800

    属性注入的几种方法

--- src/main/java/com/ltp/xml/XMLSpringCoreApplication.java
+++ src/main/java/com/ltp/xml/XMLSpringCoreApplication.java
@@ -4,7 +4,10 @@ import com.ltp.comment.Shape;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 public class XMLSpringCoreApplication {
+
+
     public static void main(String[] args) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("application-context.xml");
